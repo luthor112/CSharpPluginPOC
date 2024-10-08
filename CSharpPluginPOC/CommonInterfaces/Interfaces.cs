a034ec0323c1d55d104fbc816dd11f1dac403674
Zsolt Hornyák <kisluthor@hotmail.com>
2024-10-08 19:31:08 +0200

    MainApp + CommonInterfaces DLL + InternalPlugin

---
+++ CSharpPluginPOC/CommonInterfaces/Interfaces.cs
@@ -0,0 +1,12 @@
+﻿namespace MainApp.Interfaces
+{
+    public interface PluginInterface
+    {
+        public void CallableMethod(string inputString);
+    }
+
+    public interface UtilInterface
+    {
+        public void LogMessage(string message);
+    }
+}
---
+++ CSharpPluginPOC/MainApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using MainApp.Interfaces;
+
+namespace MainApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Main");
+
+            UtilInterface utilities = new Util();
+            PluginInterface internalPlugin = new InternalPlugin(utilities);
+            internalPlugin.CallableMethod("Test String");
+        }
+    }
+
+    public class Util : UtilInterface
+    {
+        public void LogMessage(string message)
+        {
+            Console.WriteLine("Hello from Util");
+            Console.WriteLine(message);
+        }
+    }
+
+    internal class InternalPlugin : PluginInterface
+    {
+        private UtilInterface _utilities;
+
+        public InternalPlugin(UtilInterface utilities)
+        {
+            _utilities = utilities;
+        }
+
+        public void CallableMethod(string inputString)
+        {
+            Console.WriteLine("Hello from InternalPlugin");
+            _utilities.LogMessage(inputString);
+        }
+    }
+}
