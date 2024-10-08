d625278f245c3a65afdd125e8408554a0d7fea51
Zsolt Hornyák <kisluthor@hotmail.com>
2024-10-08 19:56:37 +0200

    ExternalPlugin

---
+++ CSharpPluginPOC/ExternalPlugin/ExternalPlugin.cs
@@ -0,0 +1,20 @@
+﻿using MainApp.Interfaces;
+
+namespace ExternalPlugin
+{
+    public class ExternalPlugin : PluginInterface
+    {
+        private UtilInterface _utilities;
+
+        public ExternalPlugin(UtilInterface utilities)
+        {
+            _utilities = utilities;
+        }
+
+        public void CallableMethod(string inputString)
+        {
+            Console.WriteLine("Hello from ExternalPlugin");
+            _utilities.LogMessage(inputString);
+        }
+    }
+}
--- CSharpPluginPOC/MainApp/Program.cs
+++ CSharpPluginPOC/MainApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using MainApp.Interfaces;
+using System.Reflection;
 
 namespace MainApp
 {
@@ -9,8 +10,24 @@ namespace MainApp
             Console.WriteLine("Hello from Main");
 
             UtilInterface utilities = new Util();
+            
             PluginInterface internalPlugin = new InternalPlugin(utilities);
             internalPlugin.CallableMethod("Test String");
+
+            Assembly externalDLL = Assembly.LoadFrom("ExternalPlugin.dll");
+            foreach (Type type in externalDLL.GetExportedTypes())
+            {
+                if (type.GetInterface(typeof(PluginInterface).FullName) != null)
+                {
+                    Console.WriteLine("Found external plugin class");
+                    PluginInterface externalPlugin = Activator.CreateInstance(type, new object[] { utilities }) as PluginInterface;
+                    if (externalPlugin != null)
+                    {
+                        Console.WriteLine("Class constructed");
+                        externalPlugin.CallableMethod("Another Test String");
+                    }
+                }
+            }
         }
     }
 
